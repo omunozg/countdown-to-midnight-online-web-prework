bd1d079649487ae5569582e757136489e67281a6
Oscar Munoz <omunozg@emory.edu>
2019-03-10 05:24:42 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,18 @@
 #write your code here
 
-def countdown
+def countdown(cucu)
+  while cucu > 0 
+  puts ("#{cucu} SECOND(S)!")
+  cucu -= 1 
+end
+return "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(jin)
+  while jin > 0 
+  puts ("#{jin} SECOND(S)!")
+  jin -= 1 
+  sleep(.5)
+end
+return "HAPPY NEW YEAR!"
 end
